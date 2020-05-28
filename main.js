91dcf59c999d66d61a19aa7593965a53a8fd7ac8
Christopher Nguyen <Chris_ngu@pm.me>
2020-05-27 20:18:52 -0500

    updating file handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /*
     debug reading a folder to get music names - WIP RN
     Read folders in folders
+    load folder locations with folder option
     push to a queue to display on a screen on left hand side of screen
     Read string and split on Author and song name, read tag to see when was downloaded
 */
@@ -10,15 +11,15 @@ const url = require('url');
 const path = require('path');
 const fs = require('fs');
 
-const {app, BrowserWindow, Menu, dialog} = electron;
+const { app, BrowserWindow, Menu, dialog } = electron;
 
 let mainWindow;
 
-app.on('ready', function(){
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname,'index.html'),
-        protocol:'file:',
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
         slashes: true
     }));
 
@@ -27,7 +28,6 @@ app.on('ready', function(){
 });
 
 // Menu template
-
 const indexMenuTemplate = [
     {
         label: 'Folder',
@@ -35,36 +35,69 @@ const indexMenuTemplate = [
             {
                 label: 'Select Folder',
                 click() {
-                    dialog.showOpenDialog({
-                        properties: ['openDirectory'],
-                        filters: [{
-                            name: 'Audio', extensions: ['mp3']
-                        }]
-                    })
-                    .then(result => {
-                        console.log(result.filePaths);
-                        let loadedSongs = [];
-
-                        fs.readdir(result.filePaths.toString(), function(err, files) {
-                            if (err) {
-                                return console.log('Reading directory ERROR: ' + err);
-                            }
-                            files.forEach(function(file) {
-                                if (file.endsWith('.mp3')) {
-                                    loadedSongs.push(file);        
-                                }
-                            });
-                            return loadedSongs;
-                        });
-                    })
-                    .catch(err => {
-                        console.log("Error occurred: " +err);
-                    })
+                    selectFolder();
                 }
-            },
-            {
-                label: 'label placeholder'
             }
         ]
+    },
+    {
+        label: 'Preferences',
+        click() {
+            /*
+                Discord integration
+                Theme
+                Player size
+                bug report
+            */
+        }
+    },
+    {
+        label: 'Info',
+        click () {
+            /*
+                Show my contact information (github + linkedin)
+                Show repository
+            */
+        }
     }
 ];
+
+const selectFolder = () => {
+    //Will need to redo this into an object (key:value);
+    //Will load songs by string concatination (result.filePaths.toString() + "\\" + SONGNAMEHERE);
+    let loadedSongs = [];
+
+    dialog.showOpenDialog({
+        properties: ['openDirectory'],
+        filters: [{
+            name: 'Audio', extensions: ['mp3']
+        }]
+    })
+        .then(result => {
+            //console.log(result.filePaths);
+
+            fs.readdir(result.filePaths.toString(), function (err, files) {
+                if (err) {
+                    return console.log('Reading directory ERROR: ' + err);
+                }
+                else {
+                    files.forEach(function (file) {
+                        if (file.endsWith('.mp3')) {
+                            loadedSongs.push(file);
+                        }
+                    });
+                    
+                    /*
+                        Handle files here
+                    */
+                   for (files in loadedSongs) {
+                       console.log(loadedSongs[files]);
+                   }
+                }
+            });
+        })
+        .catch(err => {
+            console.log("Error occurred: " + err);
+        });
+
+}
