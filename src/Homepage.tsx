fcd5b4a7d0b6d7446453630a9762aed6bbb7c038
Christopher Nguyen <Chris_ngu@pm.me>
2021-04-13 19:24:46 -0500

    drag and drop fix

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,43 +1,74 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
+import { Song, SOURCE, DESTINATION } from "./types";
+
 import "./styles/pages/Homepage.css";
 
 const Homepage = () => {
-  const [sourceAudioFiles, setSourceAudioFiles] = useState(0);
+  const [sourceAudioFiles, setSourceAudioFiles] = useState(1);
   const [sourceDirectory, setSourceDirectory] = useState("SourcePlaceholder");
-  const [sourceDirectoryArray, setSourceDirectoryArray] = useState([
-    { name: "learn angular" },
-    { name: "React" }
+  const [sourceDirectoryArray, setSourceDirectoryArray] = useState<Song[]>([
+    { name: "someSongName1", path: "somePath", id: 3 },
   ]);
 
-  const [destinationAudioFiles, setDestinationFiiles] = useState(9);
+  const [destinationAudioFiles, setDestinationAudioFiles] = useState(1);
   const [destinationDirectory, setDestinationDirectory] = useState("DestinationPlaceholder");
-  const [destinationDirectoryArray, setDestinationDirectoryArray] = useState([
-    { name: "something" }
+  const [destinationDirectoryArray, setDestinationDirectoryArray] = useState<Song[]>([
+    { name: "someSongName2", path: "somePath2", id: 1 }
   ]);
+
+  // offset for destination ids
+  const idOffset = 100000;
 
   const onDrop = (e: any, location: string) => {
-    let id = e.dataTransfer.getData("id");
+    let id = parseInt(e.dataTransfer.getData("id"));
 
     if (location === "Source") {
-      // Remove from Source Array
-      // Push to Destination Array
-      let index = sourceDirectoryArray.indexOf(id);
+      // destination => source
+      for (let i = 0; i < destinationDirectoryArray.length; i++) {
+        if (destinationDirectoryArray[i].id === id) {
+          const obj: Song = {
+            id: destinationDirectoryArray[i].id,
+            path: destinationDirectoryArray[i].path,
+            name: destinationDirectoryArray[i].name
+          };
 
-      setSourceDirectoryArray([...sourceDirectoryArray.splice(index, 1), { name: id }]);
-      // sourceDirectoryArray.splice(index, 1);
-      setDestinationDirectoryArray([...destinationDirectoryArray, { name: id }]);
-      // destinationDirectoryArray.push({
-      //   name: id
-      // });
+          setSourceDirectoryArray([...sourceDirectoryArray, obj]);
+          setSourceAudioFiles(sourceAudioFiles + 1);
+          setDestinationDirectoryArray(destinationDirectoryArray.filter((item) => item.id !== id))
+          setDestinationAudioFiles(destinationAudioFiles - 1);
+          break;
+        }
+      }
     }
     else if (location === "Destination") {
-      let index = destinationDirectoryArray.indexOf(id);
-      setDestinationDirectoryArray([...destinationDirectoryArray.splice(index, 1), { name: id }]);
+      // Dropping into destination
+      for (let i = 0; i < sourceDirectoryArray.length; i++) {
+        if (sourceDirectoryArray[i].id === id) {
+          const obj = {
+            id: sourceDirectoryArray[i].id,
+            path: sourceDirectoryArray[i].path,
+            name: sourceDirectoryArray[i].name
+          };
 
-      // destinationDirectoryArray.splice(index, 1);
-      setSourceDirectoryArray([...sourceDirectoryArray, { name: id }]);
+          setDestinationDirectoryArray([...destinationDirectoryArray, obj]);
+          setDestinationAudioFiles(destinationAudioFiles + 1);
+          setSourceDirectoryArray(sourceDirectoryArray.filter((item) => item.id !== id))
+          setSourceAudioFiles(sourceAudioFiles - 1);
+          break;
+        }
+
+      }
+    }
+  }
+
+  const DirectoryLoad = (location: string) => {
+    if (location === SOURCE) {
+      // call ipc for source songs
+    }
+    if (location === DESTINATION) {
+      // call ipc for destination songs
     }
   }
 
@@ -56,16 +87,16 @@ const Homepage = () => {
           <div
             className="song-container droppable"
             onDragOver={(e) => { e.preventDefault() }}
-            onDrop={(e) => onDrop(e, "Source")}
+            onDrop={(e) => onDrop(e, SOURCE)}
           >
             {
-              sourceDirectoryArray.map((item) => {
+              sourceDirectoryArray.map((item, i) => {
                 return (
                   <div
                     className="draggable"
-                    key={item.name}
+                    key={item.id}
                     draggable
-                    onDragStart={(e) => e.dataTransfer.setData("id", item.name)}
+                    onDragStart={(e) => e.dataTransfer.setData("id", (item.id).toString())}
                   >
                     <h4>
                       {item.name}
@@ -75,25 +106,25 @@ const Homepage = () => {
               })
             }
           </div>
-          <Button variant="primary">Select Source Directory</Button>
+          <Button variant="primary" onClick={() => DirectoryLoad(SOURCE)}>Select Source Directory</Button>
         </div>
+
         <div id="right-box">
           <p>Audio files here: {destinationAudioFiles}</p>
           <p>Destination directory: {destinationDirectory}</p>
           <div
             className="song-container droppable"
             onDragOver={(e) => { e.preventDefault() }}
-            onDrop={(e) => onDrop(e, "Destination")}
-
+            onDrop={(e) => onDrop(e, DESTINATION)}
           >
             {
               destinationDirectoryArray.map((item) => {
                 return (
                   <div
                     className="draggable"
-                    key={item.name}
+                    key={item.id}
                     draggable
-                    onDragStart={(e) => e.dataTransfer.setData("id", item.name)}
+                    onDragStart={(e) => e.dataTransfer.setData("id", (item.id).toString())}
                   >
                     <h4>
                       {item.name}
@@ -103,7 +134,7 @@ const Homepage = () => {
               })
             }
           </div>
-          <Button variant="primary">Select Destination Directory</Button>
+          <Button variant="primary" onClick={() => DirectoryLoad(DESTINATION)}>Select Destination Directory</Button>
         </div>
       </div>
     </div >
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Song {
+    path: string,
+    name: string,
+    id: number
+}
+
+export const SOURCE = "Source";
+export const DESTINATION = "Destination";
